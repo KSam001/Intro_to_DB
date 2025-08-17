e995eddf0dd4fbb6d70108f01d8d8fc685cd8140
KSam001 <samcapital.inc@gmail.com>
2025-08-17 14:30:39 -0700

    feat: Add full and correct database schema for all tables

--- task_2.sql
+++ task_2.sql
@@ -1,41 +1,47 @@
+-- Create database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS ALX_BOOK_STORE;
+
+-- Use the database
+USE ALX_BOOK_STORE;
+
 -- Create the Authors table
-CREATE TABLE Authors (
-    author_id INT PRIMARY KEY,
-    author_name VARCHAR(255) NOT NULL
+CREATE TABLE AUTHORS (
+    AUTHOR_ID INT PRIMARY KEY,
+    AUTHOR_NAME VARCHAR(255) NOT NULL
 );
 
 -- Create the Books table
-CREATE TABLE Books (
-    book_id INT PRIMARY KEY,
-    title VARCHAR(130) NOT NULL,
-    author_id INT,
-    price DOUBLE,
-    publication_date DATE,
-    FOREIGN KEY(author_id) REFERENCES Authors(author_id)
+CREATE TABLE BOOKS (
+    BOOK_ID INT PRIMARY KEY,
+    TITLE VARCHAR(130) NOT NULL,
+    AUTHOR_ID INT,
+    PRICE DOUBLE,
+    PUBLICATION_DATE DATE,
+    FOREIGN KEY(AUTHOR_ID) REFERENCES AUTHORS(AUTHOR_ID)
 );
 
 -- Create the Customers table
-CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) NOT NULL,
-    address TEXT
+CREATE TABLE CUSTOMERS (
+    CUSTOMER_ID INT PRIMARY KEY,
+    CUSTOMER_NAME VARCHAR(215) NOT NULL,
+    EMAIL VARCHAR(215) NOT NULL,
+    ADDRESS TEXT
 );
 
 -- Create the Orders table
-CREATE TABLE Orders (
-    order_id INT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE NOT NULL,
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+CREATE TABLE ORDERS (
+    ORDER_ID INT PRIMARY KEY,
+    CUSTOMER_ID INT,
+    ORDER_DATE DATE NOT NULL,
+    FOREIGN KEY(CUSTOMER_ID) REFERENCES CUSTOMERS(CUSTOMER_ID)
 );
 
 -- Create the Order_Details table
-CREATE TABLE Order_Details (
-    orderdetailid INT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
-    quantity DOUBLE,
-    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY(book_id) REFERENCES Books(book_id)
+CREATE TABLE ORDER_DETAILS (
+    ORDERDETAILID INT PRIMARY KEY,
+    ORDER_ID INT,
+    BOOK_ID INT,
+    QUANTITY DOUBLE,
+    FOREIGN KEY(ORDER_ID) REFERENCES ORDERS(ORDER_ID),
+    FOREIGN KEY(BOOK_ID) REFERENCES BOOKS(BOOK_ID)
 );
