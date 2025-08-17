44c0a484739b130969e3de24f6396f6590f05985
KSam001 <samcapital.inc@gmail.com>
2025-08-17 09:30:26 -0700

    Final vim attempt: Corrected foreign key constraint on Orders table.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -37,7 +37,7 @@ CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 --
 -- Table structure for `Order_Details`
