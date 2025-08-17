46a1c8f8c0bc17f0f5aaad42d582c8578d9283c6
KSam001 <samcapital.inc@gmail.com>
2025-08-17 13:18:19 -0700

    feat: Add script to create database and SQL schema

---
+++ MySQLServer.py
@@ -0,0 +1,31 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+DB_HOST = 'localhost'
+DB_USER = 'root'
+DB_PASSWORD = 'your_new_password'
+
+DB_NAME = 'alx_book_store'
+
+def create_database():
+    try:
+        cn = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASSWORD
+        )
+        cursor = cn.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS {}".format(DB_NAME))
+        print(f"Database '{DB_NAME}' created successfully!")
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        else:
+            print(err)
+    finally:
+        if 'cn' in locals() and cn.is_connected():
+            cursor.close()
+            cn.close()
+
+if __name__ == "__main__":
+    create_database()
